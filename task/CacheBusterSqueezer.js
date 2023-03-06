6f74f780ff881b1f2c9fa491d65fcbb5c3d18f94
Thales Batista <thalesvb@live.com>
2023-03-06 23:58:48 +0000

    Build: add task to squeeze CacheBuster info file. Removes entries that were ommited from Build output.

---
+++ task/CacheBusterSqueezer.js
@@ -0,0 +1,21 @@
+/**
+ * Trim CacheBuster info file by removing entries related to files omitted from build.
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = async function ChacheBusterSqueezer({ workspace, dependencies, taskUtil, options }) {
+  const config = options.configuration;
+  const cachebusterInfoResources = await workspace.byGlob("**/sap-ui-cachebuster-info.json");
+  if (cachebusterInfoResources.length !== 1) {
+    return;
+  }
+  const cachebusterInfoRes = cachebusterInfoResources[0];
+  let cachebusterInfo = JSON.parse(await cachebusterInfoRes.getString());
+
+  for (var resourcePath in cachebusterInfo) {
+    let resource = await workspace.byPath("/resources/"+options.projectNamespace+"/"+resourcePath);
+    if(taskUtil.getTag(resource, taskUtil.STANDARD_TAGS.OmitFromBuildResult)) {
+      delete cachebusterInfo[resourcePath];
+    }
+  }
+  cachebusterInfoRes.setString(JSON.stringify(cachebusterInfo, null, 2));
+};
